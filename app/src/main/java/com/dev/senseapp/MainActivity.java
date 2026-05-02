d879394ffad86942dbdc5a325b68c602b6865da0
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2026-05-02 11:01:27 -0500

    feat: update MainActivity layout and add buttons for hardware and sensor interactions

--- app/src/main/java/com/dev/senseapp/MainActivity.java
+++ app/src/main/java/com/dev/senseapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dev.senseapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnHardware;
+    Button btnSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnHardware = findViewById(R.id.btnHardware);
+        btnSensor = findViewById(R.id.btnSensor);
+
+        btnHardware.setOnClickListener(v -> goToHardwareActivity());
+        btnSensor.setOnClickListener(v -> goToSensorActivity());
+
+        //btnSensor.setOnClickListener(v -> goToActivity(SensorActivity));
     }
+
+    private void goToHardwareActivity() {
+        Intent intent = new Intent(this, HardwareActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToSensorActivity() {
+        Intent intent = new Intent(this, SensorActivity.class);
+        startActivity(intent);
+    }
+
+/*    private void goToActivity(AppCompatActivity activity) {
+        Intent intent = new Intent(this, activity.class);
+        startActivity(intent);
+    }*/
 }
