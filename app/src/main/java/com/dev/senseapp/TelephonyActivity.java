9bdaa572a4bff8b537500fadbff55455d4d1f1cf
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2026-05-16 07:00:56 -0500

    feat: add TelephonyActivity with layout and permissions for phone state access

--- app/src/main/java/com/dev/senseapp/TelephonyActivity.java
+++ app/src/main/java/com/dev/senseapp/TelephonyActivity.java
@@ -1,17 +1,22 @@
 package com.dev.senseapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.Button;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TelephonyActivity extends AppCompatActivity {
 
-
+    TelephonyManager telephonyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,24 @@ public class TelephonyActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        requestPermissions();
+        initManager();
+
+        String operator = telephonyManager.getNetworkOperatorName();
+        String country = telephonyManager.getNetworkCountryIso();
+
+        Log.d("TelephonyActivity", "Mi operador es " + operator);
+        Log.d("TelephonyActivity", "Mi país es " + country);
+    }
+
+    private void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1001);
+        }
+    }
+
+    private void initManager() {
+        telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
     }
 }
