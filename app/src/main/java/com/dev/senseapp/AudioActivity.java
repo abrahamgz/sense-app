2d235a00b88a1339b002d990da1e6460054c28c2
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2026-05-17 11:45:30 -0500

    feat: implement AudioActivity with volume control and layout enhancements

--- app/src/main/java/com/dev/senseapp/AudioActivity.java
+++ app/src/main/java/com/dev/senseapp/AudioActivity.java
@@ -1,6 +1,10 @@
 package com.dev.senseapp;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AudioActivity extends AppCompatActivity {
+
+    private SeekBar sbMusic;
+    private TextView tvMusicVol;
+
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,57 @@ public class AudioActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        initManager();
+        initViews();
+        configurateMusicSeekBar();
+
+        /*
+        int musicVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int maxMusicVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int minMusicVol = audioManager.getStreamMinVolume(AudioManager.STREAM_MUSIC);
+
+        int alarmVol = audioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+        int maxAlarmVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
+        int minAlarmVol = audioManager.getStreamMinVolume(AudioManager.STREAM_ALARM);
+
+        Log.d("AudioActivity", "El volumen de la musica es (" + musicVol + "/" + maxMusicVol + "), el mínimo es " + minMusicVol);
+        Log.d("AudioActivity", "El volumen de la alarma es (" + alarmVol + "/" + maxAlarmVol + "), el mínimo es " + minAlarmVol);
+        */
+    }
+
+    private void initManager() {
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    private void initViews() {
+        tvMusicVol = findViewById(R.id.tvMusicVol);
+        sbMusic = findViewById(R.id.sbMusic);
+    }
+
+    private void configurateMusicSeekBar() {
+        int currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        sbMusic.setMax(maxVol);
+        sbMusic.setProgress(currentVol);
+        tvMusicVol.setText(currentVol + "/" + maxVol); // 7/15
+
+        sbMusic.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int newVol, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVol, 0);
+                tvMusicVol.setText(newVol + "/" + maxVol);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
     }
 }
