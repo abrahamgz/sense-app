5dc6e9164d85507fc0afd40ce2e2d2546b79d90c
Abraham Guzman <milton.guzman@tecsup.edu.pe>
2026-05-24 09:32:19 -0500

    feat: add TestActivity with broadcast receiver for charger connection

---
+++ app/src/main/java/com/dev/senseapp/TestActivity.java
@@ -0,0 +1,43 @@
+package com.dev.senseapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TestActivity extends AppCompatActivity {
+    private static final String TAG = "TestActivity";
+
+    private final BroadcastReceiver chargerReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "Llegó un broadcast con action: " + action);
+            Toast.makeText(context, "Cargador conectado", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_test);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_POWER_CONNECTED);
+        registerReceiver(chargerReceiver, filter);
+    }
+}
